d630bf3cfc2003d110823b88ed6e5dca4ec4a41b
bulricht <108867473+bulricht@users.noreply.github.com>
2024-04-16 12:23:37 +0200

    feat: watch config file for changes

--- pysembench/core.py
+++ pysembench/core.py
@@ -1,13 +1,39 @@
 import logging
 import os
+import time
 
 import yaml
 from apscheduler.schedulers.blocking import BlockingScheduler
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 from pysembench.dispatcher import TaskDispatcher
 from pysembench.task import Task
 
 logger = logging.getLogger(__name__)
+
+
+class ConfigFileEventHandler(FileSystemEventHandler):
+    def __init__(self, sembench_config_path, func):
+        super().__init__()
+        self.sembench_config_path = sembench_config_path
+        self.func = func
+        os.environ["PYSEMBENCH_WATCHDOG_TIME"] = "0"
+
+    def on_modified(self, event):
+        if event.src_path == self.sembench_config_path:
+            time_elapsed = time.time() - float(
+                os.environ["PYSEMBENCH_WATCHDOG_TIME"]
+            )
+            if time_elapsed < 2:
+                pass
+            else:
+                try:
+                    self.func()
+                except Exception as e:
+                    raise e
+                finally:
+                    os.environ["PYSEMBENCH_WATCHDOG_TIME"] = str(time.time())
 
 
 class Sembench:
@@ -19,6 +45,7 @@ class Sembench:
         sembench_config_path=None,
         sembench_config_file_name=None,
         scheduler_interval_seconds=None,
+        watch_config_file=False,
         fail_fast=False,
     ):
         """Create a Sembench object.
@@ -39,6 +66,7 @@ class Sembench:
 
         :returns: None
         """
+        assert input_data_location, "input_data_location must be specified"
         self.input_data_location = input_data_location
         self.output_data_location = output_data_location or input_data_location
         self.sembench_data_location = (
@@ -47,15 +75,8 @@ class Sembench:
         self.sembench_config_path = sembench_config_path
         self.sembench_config_file_name = sembench_config_file_name
         self.scheduler_interval_seconds = scheduler_interval_seconds
+        self.watch_config_file = watch_config_file
         self.fail_fast = fail_fast
-
-        if (
-            self.scheduler_interval_seconds is not None
-            and self.scheduler_interval_seconds != ""
-        ):
-            self.scheduler_interval_seconds = int(
-                self.scheduler_interval_seconds
-            )
 
         assert not (self.sembench_config_path and sembench_config_file_name), (
             "sembench_config_file_name can't be specified when "
@@ -104,12 +125,42 @@ class Sembench:
                     raise e
 
     def process(self):
-        self._process()
-        if self.scheduler_interval_seconds:
-            scheduler = BlockingScheduler()
-            scheduler.add_job(
+        if self.watch_config_file:
+            config_file_event_handler = ConfigFileEventHandler(
+                self.sembench_config_path,
                 self._process,
-                "interval",
-                seconds=self.scheduler_interval_seconds,
             )
-            scheduler.start()
+            observer = Observer()
+            observer.schedule(
+                event_handler=config_file_event_handler,
+                path=self.sembench_data_location,
+            )
+            observer.start()
+
+            try:
+                self._process()
+                if self.scheduler_interval_seconds:
+                    scheduler = BlockingScheduler()
+                    scheduler.add_job(
+                        self._process,
+                        "interval",
+                        seconds=int(self.scheduler_interval_seconds),
+                    )
+                    scheduler.start()
+                else:
+                    while True:
+                        pass
+            except KeyboardInterrupt:
+                observer.stop()
+            observer.join()
+
+        else:
+            self._process()
+            if self.scheduler_interval_seconds:
+                scheduler = BlockingScheduler()
+                scheduler.add_job(
+                    self._process,
+                    "interval",
+                    seconds=int(self.scheduler_interval_seconds),
+                )
+                scheduler.start()
